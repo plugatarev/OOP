31e2fc87ae3ff826bb6ac80a0dc8be8811c99b7e
plugatarev <k.plugatarev@g.nsu.ru>
2021-09-13 19:26:11 +0700

    blanks are made

---
+++ Lab1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+//int main(int argc, char** argv){
+//}
+
+int main(int argc, char** argv) {
+    return RUN_ALL_TESTS();
+}
