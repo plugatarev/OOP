7163c39775788f74a9284c22faf8380aba3a21e4
plugatarev <k.plugatarev@g.nsu.ru>
2021-09-18 21:24:22 +0700

    added copy constructor

--- Lab1/HashTable.cpp
+++ Lab1/HashTable.cpp
@@ -2,3 +2,12 @@
 
 HashTable::HashTable():list_(new Value[_DEFAULT_HASTABLE_SIZE]), capacity_(_DEFAULT_HASTABLE_SIZE){}
 
+HashTable::~HashTable(){
+    delete [] list_;
+}
+HashTable::HashTable(const HashTable& b):list_(new Value[b.capacity_]),capacity_(b.capacity_){
+    for (size_t i = 0; i < capacity_; i++){
+        list_[i].age = b.list_[i].age;
+        list_[i].name = b.list_[i].name;
+    }
+}
