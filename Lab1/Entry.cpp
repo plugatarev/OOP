b5d4f8364b1ce849a2683c51669cde74ec942703
plugatarev <k.plugatarev@g.nsu.ru>
2021-11-07 18:05:02 +0700

    Fixed style Entry

--- Lab1/Entry.cpp
+++ Lab1/Entry.cpp
@@ -1,10 +1,10 @@
 #include "Entry.hpp"
 
-Entry::Entry(const Entry& other):key(other.key), value(other.value){};
+Entry::Entry(const Entry& other):key(other.key), value(other.value){}
 
-Entry::Entry(Key& k, Value& v, Entry* next): key(k), value(v), next(next){};
+Entry::Entry(Key& k, Value& v, Entry* next): key(k), value(v), next(next){}
 
-Entry::~Entry(){};
+Entry::~Entry(){}
 
 bool Entry::operator==(const Entry& p){
     if (key == p.key && value == p.value) return true;
