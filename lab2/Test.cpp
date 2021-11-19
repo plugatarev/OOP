78e9f861a18783a89838f32e445e88042ee524ad
plugatarev <k.plugatarev@g.nsu.ru>
2021-11-19 19:00:42 +0700

    Added new Tests

--- lab2/Test.cpp
+++ lab2/Test.cpp
@@ -3,14 +3,14 @@
 
 Interpreter interpreter = Interpreter::getInstance();
 
-TEST(InterpreterTest, AddingAnValueToTheStack){
+TEST(InterpreterTest, CheckingPutAnValueToTheStack){
     std::string cmds = "134";
     interpreter.interpret(cmds);
     ASSERT_EQ(interpreter.get_value().top(),134);
 
 }
 
-TEST(InterpreterTest, AddingSomeValuesToTheStack){
+TEST(InterpreterTest, CheckingPutSomeValuesToTheStack){
     std::string cmds = "134 12 21 43";
     interpreter.interpret(cmds);
     ASSERT_EQ(interpreter.get_value().top_and_pop(),134);
@@ -19,4 +19,106 @@ TEST(InterpreterTest, AddingSomeValuesToTheStack){
     ASSERT_EQ(interpreter.get_value().top_and_pop(),43);
 }
 
+TEST(InterpreterTest, CheckingAddingElements){
+    std::string cmds = "134 12 21 43 +";
+    interpreter.interpret(cmds);
 
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),146);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),21);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),43);
+}
+
+TEST(InterpreterTest, CheckingSubtractionElements){
+    std::string cmds = "134 12 -";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),122);
+}
+
+TEST(InterpreterTest, CheckingMultiplicationElements){
+    std::string cmds = "2 35 *";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),70);
+}
+
+//Tests with overflow int work of arithmetic
+
+TEST(InterpreterTest, CheckingDivisionElements){
+    std::string cmds = "24 8 /";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),3);
+}
+
+//Test for division on 0
+
+TEST(InterpreterTest, CheckingModElements){
+    std::string cmds = "24 8 mod";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),0);
+}
+
+TEST(InterpreterTest, CheckingOperatorMoreForElements){
+    std::string cmds = "24 8 >";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),1);
+    std::string cmds = "5 8 >";
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),0);
+}
+
+TEST(InterpreterTest, CheckingOperatorLessForElements){
+    std::string cmds = "24 8 <";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),0);
+    std::string cmds = "5 8 <";
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),1);
+}
+
+TEST(InterpreterTest, CheckingOperatorEqualForElements){
+    std::string cmds = "24 8 =";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),0);
+    std::string cmds = "5 5 =";
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),1);
+}
+
+TEST(InterpreterTest, CheckingWritingElementOfConsole){
+    std::string cmds = "12321";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),12321);
+    
+}
+
+TEST(InterpreterTest, CheckingDupMethod){
+    std::string cmds = "11 dup";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),11);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),11);    
+}
+
+TEST(InterpreterTest, CheckingDropMethod){
+    std::string cmds = "11 123 drop";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),11);    
+}
+
+TEST(InterpreterTest, CheckingSwapMethod){
+    std::string cmds = "11 4 swap";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),11);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),4);    
+}
+
+TEST(InterpreterTest, CheckingRotMethod){
+    std::string cmds = "1 2 3 rot";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),1);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),2);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),3);    
+}
+/*
+TEST(InterpreterTest, CheckingOverMethod){
+    std::string cmds = "1 2 3 rot";
+    interpreter.interpret(cmds);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),1);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),2);
+    ASSERT_EQ(interpreter.get_value().top_and_pop(),3);    
+}*/
