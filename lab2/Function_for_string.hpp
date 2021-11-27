5935633d0b0202ae667bed451d22a9c78deb1ebd
plugatarev <k.plugatarev@g.nsu.ru>
2021-11-27 19:48:56 +0700

    Added functions for string in namespace

--- lab2/Function_for_string.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <iostream>
-bool is_number(const std::string& s);
-bool is_write_str(std::string& s);
--- lab2/Interpreter.cpp
+++ lab2/Interpreter.cpp
@@ -2,8 +2,36 @@
 #include <map>
 #include <sstream>
 #include "Exception.hpp"
-#include "Function_for_string.hpp"
+//#include "Function_for_string.hpp"
+namespace{
+    bool is_write_str(std::string& s){
+        std::string k;
+        std::string::iterator it = s.begin();
+        std::string::iterator end = s.end();
+        bool flag = 0;
+        it++;
+        if ((*it) != '"') return false;
+        it++;
+        while (it != end){
+            if ((*it) == '"') {
+                flag = 1;
+                break;
+            }
+            k+=(*it);
+            it++;
+        }
+        if (flag = 0 || (++it) != end) return false;
+        s = k;
+        return true;
+    }
 
+    bool is_number(const std::string& s){
+    std::string::const_iterator it = s.begin();
+    std::string::const_iterator end = s.end();
+    while (it != end && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+}
 Command * Interpreter::get_cmd(std::string::iterator & it, std::string::iterator & end) {
 
     std::string cmd;
