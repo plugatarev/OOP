1fdec4fa80908e2ed80486951af11939f8789e44
plugatarev <k.plugatarev@g.nsu.ru>
2021-12-01 19:55:55 +0700

    Changed My_Stack

--- lab2/My_Stack.cpp
+++ lab2/My_Stack.cpp
@@ -1,28 +1,37 @@
 #include "My_Stack.hpp"
 #include "Exception.hpp"
 
-int& My_Stack::top_and_pop(){
-    if (empty()) throw interpreter_error("extracting from an empty stack");
-    int& r = top();
+int My_Stack::peek(){
+    return stack.top();
+}
+
+int& My_Stack::pop(){
+    if (stack.empty()) throw interpreter_error("extracting from an empty stack");
+    int& r = stack.top();
     pop();
     return r;
 }
 
 void My_Stack::print(){
     My_Stack tmp;
-    while (!empty()){
-        tmp.push(top());
-        std::cout << top_and_pop() << " ";
+    while (!stack.empty()){
+        tmp.push(stack.top());
+        std::cout << pop() << " ";
     }
-    while(!tmp.empty()){
-        push(tmp.top_and_pop());
+    while(!tmp.stack.empty()){
+        push(tmp.pop());
     }
 }
+
 int My_Stack::size(){
     int c = 0;
-    while (!empty()){
-        top_and_pop();
+    while (!stack.empty()){
+        pop();
         c++;
     }
     return c;
+}
+
+void My_Stack::push(int value){
+    stack.push(value);
 }
--- lab2/My_Stack.hpp
+++ lab2/My_Stack.hpp
@@ -1,13 +1,19 @@
 #pragma once
 #include <stack>
-class My_Stack: public std::stack<int>{
+class My_Stack{
 public:
   //Returns top of the stack and remove from the stack
-  int& top_and_pop();
+  int& pop();
 
   //Prints the stack 
   void print();
 
   //Returns number of elements on the stack
   int size();
+
+  int peek();
+
+  void push(int value);
+private:
+  std::stack<int> stack;
 };
