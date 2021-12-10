8c087868be0176b273b330ecf5c785802b006daa
plugatarev <k.plugatarev@g.nsu.ru>
2021-12-10 19:54:42 +0700

    Changed some methods after cr

--- lab2/Interpreter.cpp
+++ lab2/Interpreter.cpp
@@ -1,26 +1,8 @@
 #include "Interpreter.hpp"
 #include <map>
 #include<algorithm>
-#include "Exception.hpp"
+#include "Interpreter_error.hpp"
 namespace{
-    bool is_write_str(std::string& s, std::string::iterator it, std::string::iterator & end){
-        std::string k;
-        if ((*it) != '.' || it == end) return false;
-        it++;
-        if ((*it) != '"' || it == end) return false;   
-        it++; 
-        int count = 0;
-        while (it != end && (*it) != '"' && (count++) < 10){
-            k+=(*it);
-            it++;
-        }
-        if ((*it) == '"'){
-            s = k;
-            return true;
-        }
-        return false;
-    }
-
     bool is_number(const std::string& s){
         // -344
         std::string::const_iterator it = s.begin();
@@ -29,16 +11,18 @@ namespace{
     }
 }
 
-Interpreter::Interpreter(Interpreter& other):_creators(other._creators),value(other.value){}
+Interpreter::Interpreter(Interpreter& other):_creators(std::move(other._creators)),value(other.value){}
 
-std::shared_ptr<Command> Interpreter::get_cmd(std::string::iterator & it, std::string::iterator & end) {
+std::unique_ptr<Command>* Interpreter::get_cmd(std::string::iterator & it, std::string::iterator & end) {
     std::string cmd;
     std::string::iterator tmp = it;
     char balance = 0;
     while (it != end && (*it != ' ' || (cmd[0] == '.' && cmd[1] == '\"'))){
-        // ." fff"    " bar
         if (*it == '"'){
-            if (++balance == 2) break;
+            if (++balance == 2){
+                cmd += (*it);
+                break;
+            } 
         }
         cmd += (*it);
         it++;
@@ -46,13 +30,20 @@ std::shared_ptr<Command> Interpreter::get_cmd(std::string::iterator & it, std::s
 
     if (cmd.length() == 0) return nullptr;
 
-    if (cmd.size() > 3 && cmd[0] == '.' && cmd[1] == '\"' && cmd[cmd.size() - 1]) {
-        std::cout << cmd.substr(2, cmd.size() - 1) << std::endl;
+    if (cmd.size() >= 3 && cmd[0] == '.' && cmd[1] == '\"' && cmd[cmd.size() - 1] == '\"') {
+        std::cout << cmd.substr(2, cmd.size() - 3) << std::endl;
         return nullptr;
     }
 
 
-    if (is_number(cmd)){
+    if (cmd[0] == '-' && cmd.size() > 1){
+        cmd = cmd.substr(1,cmd.size());
+        if (is_number(cmd)){
+            value.push(std::stoi(cmd) * -1);
+            return nullptr;
+        }
+    }
+    else if (is_number(cmd)){
         value.push(std::stoi(cmd));
         return nullptr;
     }
@@ -62,18 +53,19 @@ std::shared_ptr<Command> Interpreter::get_cmd(std::string::iterator & it, std::s
         ss << "no such command: '" << cmd << "'";
         throw interpreter_error(ss.str());
     }
-    return creator_it->second;
+    return &(creator_it->second);
 }
 
-void Interpreter::interpret(std::string & cmds) {
+std::string Interpreter::interpret(std::string & cmds) {
+    std::string s;
     std::string::iterator it = cmds.begin();
     std::string::iterator end = cmds.end();
-    std::shared_ptr<Command> command;
+    std::unique_ptr<Command>* command;
     while (it != end) {
         try {
             command = get_cmd(it, end);
             if (command != nullptr){
-                command->apply(value, ss);
+                (*command)->apply(value, ss);
                 std::cout << ss.str() << " ";
             }
         }
@@ -84,11 +76,6 @@ void Interpreter::interpret(std::string & cmds) {
         ss.str("");
         it++;
     }
-}
-
-std::string interpret(std::string & cmds) {
-    ss;
-    return ss.str();
 }
 
 My_Stack& Interpreter::get_value(){
--- lab2/Interpreter.hpp
+++ lab2/Interpreter.hpp
@@ -4,6 +4,7 @@
 #include <map>
 #include "My_Stack.hpp"
 #include<memory>
+#include<iostream> //delete
 
 class Interpreter {
 public:
@@ -13,13 +14,12 @@ public:
       return i;
    }
    //Runs Interpreter
-   void interpret(std::string & cmds);
+   std::string interpret(std::string & cmds);
    typedef Command * (*creator_t)();
 
    //Adds new command
    bool registerCreator(const std::string& c, std::unique_ptr<Command> && command) {
-       _creators[c] = std::move(command);
-//      _creators.insert(std::make_pair(c,std::move(command)));
+      _creators[c] = std::move(command);
       return true;
    }
 
@@ -30,6 +30,6 @@ public:
 private:
    Interpreter() = default;
    std::map<std::string, std::unique_ptr<Command>> _creators;
-   std::shared_ptr<Command> get_cmd(std::string::iterator & it, std::string::iterator & end);
+   std::unique_ptr<Command>* get_cmd(std::string::iterator & it, std::string::iterator & end);
    My_Stack value;
 };
