d39b792052a56673a7acb449d6b0498bebf5dff7
plugatarev <k.plugatarev@g.nsu.ru>
2021-12-25 13:38:29 +0700

    Chenged main

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,29 +1,31 @@
 #include "variant.hpp"
 #include <variant>
 class A{};
+
+MyVariant::variant<double,int> var(){
+    MyVariant::variant<double,int> a(12);
+    return a;
+}
+
 int main(){
 
-    // std::variant<int, double, A, int&> v;
-    // std::variant<int, double, A, int&> g(v);
-    //v = 1.21;
-    //v = new A();
-    //int* b = new int;
-    int a2 = 1;
-    //MyVariant::variant<double, int> a(a2);
-    // UtilsVariant::utils<int, double, int> ac;
-    // int* b;
-    // int* b_0;
-    // std::cout << ac.move(b,b_0);
-    //My::variant<char> b(a);
-    //a = "df";
-    // std::cout << "size is " << sizeof(a) << "   value is " << a.get<int>() << std::endl;
-    // a = 23432423432.3434;
-    // std::cout << "size is " << sizeof(a) << "   value is " << a.get<double>() << std::endl;
-    //MyVariant::variant<int,char> b(a);
-    //std::cout << "size is " << sizeof(a) << "   value is " << a.get<int>() << std::endl;
-    //variant<> v;
-    // int* v = new int(3);
-    // a = v;
-    // std::cout << "size is " << sizeof(a) << "   value is " << *(a.get<int*>()) << std::endl;
+    std::string a2 = "3234";
+    MyVariant::variant<double, int, float, short, char, std::string> a(123);
+    std::cout << a.get<int>() << std::endl;
+    a = 1;
+    std::cout << "Hello\n";
+    std::cout << a.get<int>() << std::endl;
+    MyVariant::variant<double, int, float, short, char, std::string> b(a);
+    std::cout << b.get<int>() << std::endl;
+    b = 200;
+    std::cout << a.get<int>() << std::endl;
+    std::cout << b.get<int>() << std::endl;
+    a = 100;
+    std::cout << a.get<int>() << std::endl;
+    std::cout << b.get<int>() << std::endl;
+    // a = 21321.1;
+    // std::cout << a.get<double>() << std::endl;
+    // MyVariant::variant<double, int> bb(std::move(a));
+    // bb.get<int>();
     return 0;
 }
