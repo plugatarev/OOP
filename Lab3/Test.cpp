128e59b044c5a6c971d3fa7b02b93774774e8d30
plugatarev <k.plugatarev@g.nsu.ru>
2021-12-25 13:08:57 +0700

    Added some tests

--- Lab3/Test.cpp
+++ Lab3/Test.cpp
@@ -1,8 +1,78 @@
 #include <gtest/gtest.h>
 #include "variant.hpp"
 
-
-TEST(VariantTest, Test){
-     
+MyVariant::variant<double,int> var(int val){
+    MyVariant::variant<double,int> a(val);
+    return a;
 }
 
+class A{
+public:
+    A(int a):code(a){}
+    int code;
+};
+
+TEST(VariantTest, CreateEmptyVariant){
+    MyVariant::variant<double, int> a();       
+}
+
+TEST(VariantTest, CreateVariantWithString){
+    std::string s = "sdf1232";
+    MyVariant::variant<double, int, float, std::string> a(s);   
+    ASSERT_EQ(a.get<std::string>(), s); 
+}
+
+TEST(VariantTest, CreateOnBaseLValueWithATypeThatCanBeStored){
+    int value = 12321321;
+    MyVariant::variant<double, int> a(value);   
+    ASSERT_EQ(a.get<int>(), 12321321); 
+}
+
+TEST(VariantTest, CreateOnBaseRValueWithATypeThatCanBeStored){
+    MyVariant::variant<double, int> a(12);   
+    ASSERT_EQ(a.get<int>(), 12); 
+}
+
+TEST(VariantTest, CreateOnBaseRValueWithATypeForMoveConstructorThatCanBeStored){
+    MyVariant::variant<double, int> a(var(2));   
+    ASSERT_EQ(a.get<int>(), 2); 
+}
+
+TEST(VariantTest, CreateOnBaseRValueWithATypeForOperatorAssignThatCanBeStored){
+    MyVariant::variant<short, class A> a(A(123));  
+    MyVariant::variant<short, class A> b = std::move(a);
+    ASSERT_EQ(b.get<class A>().code, 123); 
+}
+
+TEST(VariantTest, CopyConstructor){
+    MyVariant::variant<double, int> a(12.12); 
+    MyVariant::variant<double, int> acopy(a);
+    ASSERT_EQ(a.get<double>(), 12.12);
+    ASSERT_EQ(acopy.get<double>(), 12.12);
+}
+
+TEST(VariantTest, GetValueWithOtherType){
+    MyVariant::variant<double, int> a(12.12); 
+    try{
+        a.get<int>();
+    }
+    catch(variant_access_error& er){
+        std::string s = "This type isn't stored in this variant";
+        ASSERT_EQ(er.what(), s);
+    }
+}
+
+TEST(VariantTest, OriginalVariantIsNotChangedOnOperatorAssign){
+    int* f = new int(1);
+    MyVariant::variant<double, int, int*> a(12.12); 
+    MyVariant::variant<double, int, int*> b = a;
+    ASSERT_EQ(a.get<double>(), 12.12);
+    ASSERT_EQ(b.get<double>(), 12.12);
+    b = 12321;
+    ASSERT_EQ(a.get<double>(), 12.12);
+    ASSERT_EQ(b.get<int>(), 12321);
+    a = f;
+    ASSERT_EQ(*(a.get<int*>()), 1);
+    ASSERT_EQ(b.get<int>(), 12321);
+    delete f;
+}
