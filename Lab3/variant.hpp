e87a6616e46246db4551007e7b3a8f1d01fd23be
plugatarev <k.plugatarev@g.nsu.ru>
2021-12-25 19:58:19 +0700

    Version with copy placeholder pointer

--- Lab3/variant.hpp
+++ Lab3/variant.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <memory>
 #include "Exception.hpp"
 #include "Is_Same.hpp"
 
@@ -24,16 +23,15 @@ namespace UtilsVariant{
             return false;
         }
     };
-
 }
 
 namespace MyVariant{
-    // At least one arg
+    // at least one arg
     template<typename F, typename... Args>
     class variant{
     public: 
         //Default constructor. data assign nullptr
-        variant():data(nullptr){}
+        variant(){}
 
         //Makes a copy of value, so that the initial content of the new instance is equivalent in both type and value to value.
         template<typename T> variant(const T& value):data(new impl(value)){
@@ -44,29 +42,31 @@ namespace MyVariant{
         template<typename T>
         variant& operator=(const T& value){
             static_assert(UtilsVariant::is_type<T, F, Args...>::is());
-            data = std::make_shared<impl<T>>(value);
+            delete data;
+            data = new impl(value);
             return *this;
         }
 
         //Copy constructor. Сannot give variant with different stored types(F, Args...).
-        variant(const variant<F,Args...>& old):data(old.data){}
+        variant(const variant<F,Args...>& old):data(copy<F, Args...>::copy_val(old.data)){}
 
         //Move constructor. Сannot give variant with different stored types(F, Args...).
         variant(variant<F,Args...>&& old){
             std::swap(data, old.data);
         }
 
-        //Serves as both the move and the copy asignment operator.
-        variant<F,Args...>& operator=(variant<F,Args...>&& old){
+        // // Serves as both the move and the copy asignment operator.
+        variant<F,Args...>& operator=(variant<F,Args...>&& old){ // - ok
             std::swap(data, old.data);
             return *this;
         }
+
         //Returns the value of the variant given the type, which can stored in this variant.
         //If the variant stores not the type T then throw variant_access_error exception with message(This type isn't stored in this variant).
         template<typename T>
         T get(){
             static_assert(UtilsVariant::is_type<T, F, Args...>::is());
-            return (dynamic_cast<impl<T>*>(data.get()) != nullptr ? dynamic_cast<impl<T>*>(data.get())->val : throw variant_access_error("This type isn't stored in this variant"));
+            return (dynamic_cast<impl<T>*>(data) != nullptr ? dynamic_cast<impl<T>*>(data)->val : throw variant_access_error("This type isn't stored in this variant"));
         }
         
         //Returns the point of value of the variant given the type, which can stored in this variant.
@@ -80,8 +80,10 @@ namespace MyVariant{
                 return nullptr;
         }
 
-        //Destructor variant
-        ~variant(){}
+        //Destructor
+        ~variant() {
+            if (data != nullptr) delete data;
+        }
 
     private: 
         struct placeholder{
@@ -96,6 +98,128 @@ namespace MyVariant{
             T val;
 
         };
-        std::shared_ptr<placeholder> data;
+
+        template<typename... Ts>
+        struct copy;
+
+        template<typename G, typename... Ts>
+        struct copy<G, Ts...> {
+            static placeholder* copy_val(placeholder* old) {
+                if (dynamic_cast<impl<G>*>(old)){
+                    return new impl<G>(*dynamic_cast<impl<G>*>(old));
+                }
+                else return copy<Ts...>::copy_val(old);
+            }   
+        };
+
+        template<typename G>
+        struct copy<G> {
+            static placeholder* copy_val(placeholder* old) {
+                if (dynamic_cast<impl<G>*>(old)){
+                    return new impl<G>(*dynamic_cast<impl<G>*>(old));
+                }
+                return nullptr;
+            }   
+        };
+        
+        placeholder* data{nullptr};
     };
 }
+
+
+//VERSION with shared_ptr
+// namespace UtilsVariant{
+//     template<typename... Args>
+//     struct is_type;
+
+//     template<typename T, typename F, typename... Args>
+//     struct is_type<T, F, Args...> {
+//         static constexpr bool is(){
+//             if (My::is_same<T,F>::value) return true;
+//             else{
+//                 if (is_type<T, Args...>::is()) return true;
+//             }
+//             return false;
+//         }
+//     };
+
+//     template<typename T> struct is_type<T>{
+//         constexpr static bool is() {
+//             return false;
+//         }
+//     };
+
+// }
+
+// namespace MyVariant{
+//     // At least one arg
+//     template<typename F, typename... Args>
+//     class variant{
+//     public: 
+//         //Default constructor. data assign nullptr
+//         variant():data(nullptr){}
+
+//         //Makes a copy of value, so that the initial content of the new instance is equivalent in both type and value to value.
+//         template<typename T> variant(const T& value):data(new impl(value)){
+//             static_assert(UtilsVariant::is_type<T, F, Args...>::is());
+//         }
+
+//         //Makes a copy of value, discarding previous content, so that the new content of is equivalent in both type and value to value.
+//         template<typename T>
+//         variant& operator=(const T& value){
+//             static_assert(UtilsVariant::is_type<T, F, Args...>::is());
+//             data = std::make_shared<impl<T>>(value);
+//             return *this;
+//         }
+
+//         //Copy constructor. Сannot give variant with different stored types(F, Args...).
+//         variant(const variant<F,Args...>& old):data(old.data){}
+
+//         //Move constructor. Сannot give variant with different stored types(F, Args...).
+//         variant(variant<F,Args...>&& old){
+//             std::swap(data, old.data);
+//         }
+
+//         //Serves as both the move and the copy asignment operator.
+//         variant<F,Args...>& operator=(variant<F,Args...>&& old){
+//             std::swap(data, old.data);
+//             return *this;
+//         }
+//         //Returns the value of the variant given the type, which can stored in this variant.
+//         //If the variant stores not the type T then throw variant_access_error exception with message(This type isn't stored in this variant).
+//         template<typename T>
+//         T get(){
+//             static_assert(UtilsVariant::is_type<T, F, Args...>::is());
+//             return (dynamic_cast<impl<T>*>(data.get()) != nullptr ? dynamic_cast<impl<T>*>(data.get())->val : throw variant_access_error("This type isn't stored in this variant"));
+//         }
+        
+//         //Returns the point of value of the variant given the type, which can stored in this variant.
+//         //If the variant stores not the type T then returns nullptr.
+//         template<typename T>
+//         T * get_if(){
+//             static_assert(UtilsVariant::is_type<T, F, Args...>::is());
+//             if (UtilsVariant::is_type<T, Args...>::is())
+//                 return dynamic_cast<impl<T>*>(data);
+//             else 
+//                 return nullptr;
+//         }
+
+//         //Destructor variant
+//         ~variant(){}
+
+//     private: 
+//         struct placeholder{
+//             virtual ~placeholder() {}
+//         };
+
+//         template < typename T >
+//         struct impl: placeholder{
+//             impl(){};
+//             impl(T const& t):val(t){}
+//             ~impl(){}
+//             T val;
+
+//         };
+//         std::shared_ptr<placeholder> data;
+//     };
+// }
